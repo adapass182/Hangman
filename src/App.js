2117c89c430cd621fe7ede0a8e46a9785ca0b063
adapass182 <adampassingham90@gmail.com>
2018-05-12 12:42:01 +0200

    Static mockup created in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="app-title">Hangman</h1>
+        <form className="guess-area">
+          <input type="text" name="Guess a letter..."/><input type="submit" value="Submit"/><h2 className="wrong-guess-count">Wrong guesses: 0</h2>
+        </form>
+        <h2 className="wordToGuess">_ _ _ _ _ _</h2>
+        <form className="new-game">
+          <input type="submit" value="New game" />
+        </form>
       </div>
     );
   }
